b918128821acdc185dae9b987a59c6f08a0f078a
Dustin Gouner <dustingouner20@gmail.com>
2023-07-10 15:13:49 -0600

    feat: add Api Call for user and user flights

--- ApiCalls.js
+++ ApiCalls.js
@@ -1,20 +1,44 @@
+const getUserFlights = () => {
+  return fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/user/1/flights')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Unable to fetch user flights. Please try again later.');
+      }
+      return response.json();
+    });
+};
 
-const getUserFlights = async () => {
-  const response = await fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/user1/flights')
-  if (!response.ok) {
-    throw new Error()
-  }
-  return await response.json()
-}
+const getUser = () => {
+  return fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/users/1')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Unable to fetch user flights. Please try again later.');
+      }
+      return response.json();
+    });
+};
 
 
 
-const getUser = async () => {
-  const response = await fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/user1')
-  if (!response.ok) {
-    throw new Error()
-  }
-  return await response.json()
-}
+// const getUserFlights = async () => {
+//   const response = await fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/user/1/flights')
+//   if (!response.ok) {
+//     throw new Error()
+//   }
+//   return await response.json()
+// }
 
-export default { getUser, getUserFlights } 
+// const getUser = async () => {
+//   fetch('https://fa654a41-d040-4528-99c2-4e7b349236b9.mock.pstmn.io/users/1')
+//   .then(response => {
+
+//     if (!response.ok) {
+//       throw new Error()
+//     }
+//     return response.json()
+//   })
+// }
+
+
+
+export { getUser, getUserFlights }
