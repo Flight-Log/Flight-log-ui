7daf14cdb3692c6751035fb9e88fbaa746bb2f0f
Cade Chaney <cadechaney@live.com>
2023-07-10 21:53:02 -0500

    feat: Add Flight Time component

--- components/FlightHistoryScreen.js
+++ components/FlightHistoryScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import FlightTime from './FlightTime'
 
 function FlightHistoryScreen() {
   return (
     <View>
+      <FlightTime />
       <Text>Flight History</Text>
       
     </View>
---
+++ components/FlightTime.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function FlightTime() {
+  return (
+    <View>
+      <Text>Flight Time</Text>
+      
+    </View>
+  )
+}
+
+export default FlightTime
