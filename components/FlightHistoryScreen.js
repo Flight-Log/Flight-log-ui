a83e9eaed4dc11faa8f873adb149cd1edc87388c
Dustin Gouner <dustingouner20@gmail.com>
2023-07-11 23:54:22 -0600

    add function to render FlightCard to flight history screen

--- components/FlightHistoryScreen.js
+++ components/FlightHistoryScreen.js
@@ -1,15 +1,57 @@
 import React, {useEffect} from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import FlightTime from './FlightTime'
+import FlightCard from './FlightCard'
+
 
 const FlightHistoryScreen = ({ route }) => {
+ 
+  // console.log(route.params.userFlights)
+  const userFlights = route.params.userFlights
+
+  const flightCards = userFlights.map((flight) => {
+    
+    const flightId = flight.id;
+    const flightDetails = flight.attributes;
+    
+    return (
+
+      <View key={flightId}>
+        <FlightCard 
+          date={flightDetails.date}
+          departure={flightDetails.start_location}
+          arrival={flightDetails.end_location}
+          aircraft={flightDetails.aircraft}
+          pilot={flightDetails.role}
+        />
+      </View>
+    )
+  })
 
   return (
-    <View>
+    <View style={[ styles.container, { flexDirection: 'column' }]}>
       <FlightTime userFlights={route.params.userFlights} />
-      <Text>This is the flight history component</Text>
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.cardsContainer}>{flightCards}</View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  cardsContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+    
+  },
+});
 
 export default FlightHistoryScreen
