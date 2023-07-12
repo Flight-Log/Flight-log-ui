10110e3b157d6ddf8019e593cded91889df3a1c2
Dustin Gouner <dustingouner20@gmail.com>
2023-07-12 00:56:44 -0600

    feat: add styling to flight history screen

--- components/FlightHistoryScreen.js
+++ components/FlightHistoryScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, ImageBackground } from 'react-native'
 import FlightTime from './FlightTime'
 import FlightCard from './FlightCard'
 
@@ -18,6 +18,7 @@ const FlightHistoryScreen = ({ route }) => {
 
       <View key={flightId}>
         <FlightCard 
+          id={flightId}
           date={flightDetails.date}
           departure={flightDetails.start_location}
           arrival={flightDetails.end_location}
@@ -29,27 +30,40 @@ const FlightHistoryScreen = ({ route }) => {
   })
 
   return (
-    <View style={[ styles.container, { flexDirection: 'column' }]}>
-      <FlightTime userFlights={route.params.userFlights} />
-      <ScrollView style={styles.scrollContainer}>
-        <View style={styles.cardsContainer}>{flightCards}</View>
-      </ScrollView>
-    </View>
+    <ImageBackground source={require('../assets/hero-img.png')} style={styles.imageBackground}>
+      <View style={[ styles.container, { flexDirection: 'column' }]}>
+        <FlightTime style={styles.timeContainer} userFlights={route.params.userFlights} />
+        <ScrollView style={styles.scrollContainer} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.cardsContainer}>{flightCards}</View>
+        </ScrollView>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+  },
   container: {
     flex: 1,
   },
+  timeContainer: {
+    flex: 2,
+  },
   scrollContainer: {
-    flex: 1,
+    flex: 3,
+  
+  },
+  contentContainer: {
+    flexGrow: 1,
   },
   cardsContainer: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 200,
+    marginTop: 10,
+    marginBottom: 10
     
   },
 });
