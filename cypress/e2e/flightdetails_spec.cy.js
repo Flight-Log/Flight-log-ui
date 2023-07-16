98ca007f710668a0546c442a12b879a6a7048eaa
Houda Jawad <houdajawad@gmail.com>
2023-07-16 11:55:20 -0700

    fix: test CI for flight detials spec

--- cypress/e2e/flightdetails_spec.cy.js
+++ cypress/e2e/flightdetails_spec.cy.js
@@ -9,7 +9,7 @@ describe('Flight Details spec', () => {
 it('Should be able to click a flight card and get directed to a details screen', () => {
   cy.visit('https://flight-log-six.vercel.app/')
   cy.get('[data-testid="view-flights-button"]').contains('View Flight History').click().url().should('eq', 'https://flight-log-six.vercel.app/')
-  cy.get(':nth-child(2) > :nth-child(1) > .r-14lw9ot').click().url().should('eq', 'https://flight-log-six.vercel.app/')
+  cy.get(':nth-child(2) > :nth-child(1) > .r-14lw9ot > :nth-child(1) > :nth-child(2) > .r-p1pxzi').click().url().should('eq', 'https://flight-log-six.vercel.app/')
       .get(':nth-child(1) > :nth-child(1) > .r-1awozwy > .css-175oi2r > :nth-child(2)').contains('Date:')
       .get(':nth-child(1) > :nth-child(1) > .r-1awozwy > .css-175oi2r > :nth-child(2)').contains('2023-05-09')
       .get(':nth-child(1) > :nth-child(1) > .r-1awozwy > .css-175oi2r > :nth-child(3)').contains('Departure')
