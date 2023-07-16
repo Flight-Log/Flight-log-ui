8d426dcb2c32669773cf1211d417ec0f5c9fae62
Houda Jawad <houdajawad@gmail.com>
2023-07-15 18:20:33 -0700

    fix: test CI

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,32 +1,32 @@
 describe('homepage spec', () => {
-  beforeEach(() => {
-    cy.visit('https://flight-log-ui-hjawad22-flightlog.vercel.app/')
+    beforeEach(() => {
+      cy.intercept('GET', 'https://flight-log-be-24cea5be4c8e.herokuapp.com/api/v1/users/1/flights/', {
+        statusCode: 200,
+        fixture: 'flights'
+      })
+    cy.visit('https://flight-log-eight.vercel.app/')
   })
 
   it('Should display logo', () => {
-    cy.get('.r-1tsn96g').contains('Flight Log')
-    // cy.get('[data-testid="logo-text"]').contains('Flight Log')
+    cy.get('[data-testid="logo-text"]').contains('Flight Log')
   })
 
   it('Should display a question to user', () => {
-    cy.get('.r-117bsoe > .css-1rynq56').contains('What would you like to do today?')
-    // cy.get('[data-testid="question-text"]').contains('What would you like to do today?')
+    cy.get('[data-testid="question-text"]').contains('What would you like to do today?')
   })
 
   it('Should display a log flight button that takes user to flight form', () => {
-    cy.get('.r-1awozwy > :nth-child(2)').contains("Log a Flight")
-    // cy.get('[data-testid="log-flight-button"]').contains('Log a Flight')
-    //  .get('[data-testid="log-flight-button"]').click().url().should('eq', 'https://flight-log-ui-hjawad22-flightlog.vercel.app/')
+    cy.get('[data-testid="log-flight-button"]').contains('Log a Flight')
+      .get('[data-testid="log-flight-button"]').click().url().should('eq', 'https://flight-log-eight.vercel.app/')
   })
 
   it('Should display a log view flight history button that takes user to flight form', () => {
-    cy.get('.r-1awozwy > :nth-child(3)').contains("View Flight History")
-    // cy.get('[data-testid="view-flights-button"]').contains('View Flight History')
-    //  .get('[data-testid="view-flights-button"]').click().url().should('eq', 'https://flight-log-ui-hjawad22-flightlog.vercel.app/')
+    cy.get('[data-testid="view-flights-button"]').contains('View Flight History')
+      .get('[data-testid="view-flights-button"]').click().url().should('eq', 'https://flight-log-eight.vercel.app/')
   })
 
-  // it('Should display a background image', () => {
-  // cy.get('[data-testid="image-background"]').should('exist').and('be.visible');
-  // })
+  it('Should display a background image', () => {
+  cy.get('[data-testid="image-background"]').should('exist').and('be.visible');
+  })
 
 })
